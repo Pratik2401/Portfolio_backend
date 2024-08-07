4dc6f292e45e3ebeee4b32ccd4cea5d5223559ef
Pratik Mali <pratikmali242005@gmail.com>
2024-08-07 20:59:04 +0530

    changes

--- server.js
+++ server.js
@@ -7,15 +7,7 @@ const errorHandler = require("./middlewares/errorHandler");
 
 
 connectDB()
-app.use(cors({ origin: 'https://pratikmali.vercel.app/admin', 
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-  },
-  { origin: 'https://pratikmali.vercel.app', 
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-  }
-))
+app.use(cors())
 app.use(express.json())
 app.use("/api/messages", require("./routes/messageRoutes"));
 app.use("/api/admin", require("./routes/adminRoutes"));
