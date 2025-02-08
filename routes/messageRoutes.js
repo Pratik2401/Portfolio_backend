66a22687e5e2c595206282931c245799cf6526dc
Pratik Mali <105542579+Pratik2401@users.noreply.github.com>
2025-02-09 00:54:36 +0530

    Update messageRoutes.js

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -1,11 +1,11 @@
 const express=require("express")
 const router=express.Router()
-const {getMessage,sendMessage,deleteMessage,deleteAll}=require('../controller/messageController')
+const {getMessage,sendMessage,deleteMessage,deleteAll,sendchocomail}=require('../controller/messageController')
 const validateToken=require('../middlewares/validateToken')
 
 router.route("/get").get(validateToken,getMessage)
 router.route("/send").post(sendMessage)
 router.route("/delete/:id").delete(validateToken,deleteMessage)
 router.route("/delAll").delete(validateToken,deleteAll)
-
+router.route("/send-email").delete(sendchocomail)
 module.exports=router
